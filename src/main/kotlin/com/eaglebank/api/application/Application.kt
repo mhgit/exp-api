1457841f29796403ce7bdb2451adbe3c1761b110
Martin Harris <136410+mhgit@users.noreply.github.com>
2025-06-07 00:52:30 +0100

    Add console logging.

--- src/main/kotlin/com/eaglebank/api/application/Application.kt
+++ src/main/kotlin/com/eaglebank/api/application/Application.kt
@@ -17,6 +17,12 @@ import com.eaglebank.api.infra.security.configureSecurity
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 import com.eaglebank.api.infra.di.serviceModule
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.ConsoleAppender
+import org.slf4j.LoggerFactory
 
 
 fun main() {
@@ -26,7 +32,9 @@ fun main() {
     }.start(wait = true)
 }
 
-fun Application.module() {
+fun Application.module() {configureLogging()
+    configureLogging()
+
     val applicationConfig = HoconApplicationConfig(ConfigFactory.load("application-dev.conf"))
 
     install(Koin) {
@@ -77,5 +85,28 @@ fun Application.configureRouting() {
 fun Application.configureOpenAPI() {
     routing {
         openAPI(path = "openapi", swaggerFile = "api-contract.yml")
+    }
+}
+
+fun Application.configureLogging() {
+    val rootLogger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME) as Logger
+    val context = LoggerFactory.getILoggerFactory() as LoggerContext
+    
+    val consoleAppender = ConsoleAppender<ILoggingEvent>().apply {
+        setContext(context)
+        name = "STDOUT"
+        
+        encoder = PatternLayoutEncoder().apply {
+            setContext(context)
+            pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+            start()
+        }
+        
+        start()
+    }
+    
+    rootLogger.apply {
+        level = ch.qos.logback.classic.Level.INFO
+        addAppender(consoleAppender)
     }
 }
